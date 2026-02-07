5262a0af3ac15c97bce5e851e90bc36c424c5149
Robbie Stoffel <robbiestoffel@gmail.com>
2026-02-06 22:42:01 -0500

    greets and doubles

--- cmd/hello/main.go
+++ cmd/hello/main.go
@@ -4,10 +4,24 @@ import (
 	"fmt"
 )
 
-func main() {
-	var name = "Robbie"
-	fmt.Println("Hello " + name + ".")
-	// -or-
-	name = "Stoff"
+/*
+func double(x int) int {
+	var doubled int
+	doubled = x * 2
+	return doubled
+}
+*/
+
+func double(x int) int {
+	doubled := x * 2
+	return doubled
+}
+
+func greet(name string) {
 	fmt.Printf("Hello %v.\n", name)
+}
+
+func main() {
+	greet("Robbie")
+	greet("Frankie")
 }
--- cmd/hello/main_test.go
+++ cmd/hello/main_test.go
@@ -1,9 +1,29 @@
 package main
 
-func Example_main() {
+import "fmt"
+
+func ExampleMain() {
 
 	main()
 
 	// Output:
-	// Hello world.
+	// Hello Robbie.
+	// Hello Frankie.
+}
+
+func ExampleGreet() {
+	greet("Robbie")
+
+	// Output:
+	// Hello Robbie.
+}
+
+func ExampleDouble() {
+
+	fmt.Println(double(2))
+	fmt.Println(double(9))
+
+	// Output:
+	// 4
+	// 18
 }
